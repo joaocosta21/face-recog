2e618b5250185c5b002479882b28fd290da8c9f3
joaocosta21 <joaocostared@gmail.com>
2023-08-27 03:30:44 +0100

    debug

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import math
 
 def face_confidence(face_distance, face_match_threshold=0.6):
     range = (1.0 - face_match_threshold)
-    linear_value = (1.0 - face_distance) / (range * 2.0)
+    linear_val = (1.0 - face_distance) / (range * 2.0)
 
     if face_distance > face_match_threshold:
         return str(round(linear_val * 100,2)) + '%'
